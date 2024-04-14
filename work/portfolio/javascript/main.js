1b6a201cfa90eda4b723ed8b7accf386f49ac09f
Park Juhee <vpdsnsl123@naver.com>
2024-04-15 08:05:00 +0900

    포트폴리오 작업

--- work/portfolio/javascript/main.js
+++ work/portfolio/javascript/main.js
@@ -1,2 +1,7 @@
-
-$(".loading-bar").animate({ "width": "100%" }, 2500);
+var swiper = new Swiper(".mySwiper1", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  loopFillGroupWithBlank: true, // 빈 슬라이드 추가
+  centeredSlides: true, // 슬라이드 가운데 정렬
+});
